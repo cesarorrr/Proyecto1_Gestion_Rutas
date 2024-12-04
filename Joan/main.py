880e7d19ffb3497a886b1b88deb004a9d5fb84e5
JoanPetitBa <130753607+JoanPetitBa@users.noreply.github.com>
2024-12-04 17:47:39 +0100

    Millorar algoritme

--- Joan/main.py
+++ Joan/main.py
@@ -1,9 +1,9 @@
-import sqlite3,random
-from typing import Dict, Tuple
+import sqlite3, json
 import networkx as nx
 from networkx.algorithms import approximation as approx
-import matplotlib.pyplot as plt
 from math import radians, sin, cos, sqrt, atan2
+
+############################## FUNCIONES DEL ALGORITMO ##############################
 
 # FUNCIO PER SABER LA DISTANCIA ENTRE COORDENADES
 def haversine(lat1:float, lon1:float, lat2:float, lon2:float):
@@ -63,6 +63,10 @@ def siguiente_id_camion(camiones:dict) -> int:
     else:
         return max(camiones.keys())+1
 
+########################################################################################################################
+
+############################## CLASES ##############################
+
 class Camion():
     def __init__(self,id_camion:int, id_pedidos:list, destinos:dict, id_productos:list, cantidad:int):
         self.id_camion = id_camion
@@ -70,6 +74,16 @@ class Camion():
         self.destinos = destinos # {"nombre":coordenadas}
         self.id_productos = id_productos
         self.cantidad = cantidad
+
+    def to_dict(self):
+        # Convert the object to a dictionary
+        return {
+            "id_camion": self.id_camion,
+            "id_pedidos": self.id_pedidos,
+            "destinos": self.destinos,
+            "id_productos": self.id_productos,
+            "cantidad": self.cantidad
+    }
 
     def __str__(self):
             return (f"Camion ID: {self.id_camion}\n"
@@ -97,16 +111,17 @@ class Pedido():
         )
         
 
-# -----------------------------------------------------------------------------------------------------------------------------------------#
+########################################################################################################################
+
+############################################################ MAIN ############################################################
 
 conn = sqlite3.connect(r"C:\Users\joant\OneDrive\Stucom\MasterIA\IA\Projecte 1 - Logistica\logistics.db")
 cursor = conn.cursor()
 
-###################################### ESTABLIR LES VARIABLES ########################################
+# ESTABLIR LES VARIABLES 
 velocidad_media_camiones = 100  # km/h
 capacidad_camiones = 1000      # unidades
 coste_medio_km = 0.6           # € por km
-######################################################################################################
 
 # OBTENIR LES COMANDES PER PRODUCTE
 pedidos = cursor.execute("""
@@ -157,23 +172,26 @@ for ped in pedidos:
 pedidos_pendientes = {} # ID_PEDIDO : [CANTIDAD,[COORDENADAS]]
 camiones = {} # ID_PEDIDO : {CAMION}
 
-################################################################# DEFINIR QUINS PRODUCTES VAN A QUIN CAMIÓ ##############################################################################
+############################## DEFINIR QUINS PRODUCTES VAN A QUIN CAMIÓ ##############################
 
-pedidos_to_pop = []
 
+# ITERAR POR CADA ID_PRODUCTO
 while len(pedidos_ordenados.keys()):
+
+    for k,v in list(pedidos_ordenados.items()):
+        if v == []:
+            del pedidos_ordenados[k]
 
     # ITERAR POR CADA ID_PRODUCTO
     for id_producto, pedidos_producto in list(pedidos_ordenados.items()):
         
+        pedidos_to_pop = []
+
         # ITERAR POR CADA PEDIDO DE UN ID_PRODUCTO
         for index1, pedido in enumerate(pedidos_producto):
 
             # COMPROVAR SI LA CANTITAT DEL PEDIDO ES MÉS GRAN QUE LA CAPACITAT DELS CAMIONS        
-            if pedido.cantidad >= capacidad_camiones:
-
-                # LA QUANTITAT QUE ANIRA AL CAMIÓ ES EL TOTAL DE LA SEVA CAPACITAT
-                cantidad_camion = capacidad_camiones
+            while pedido.cantidad >= capacidad_camiones:
                 
                 # CREAR EL CAMIÓ
                 camio = Camion(
@@ -181,51 +199,85 @@ while len(pedidos_ordenados.keys()):
                     id_pedidos = [pedido.id_pedido],
                     destinos = {pedido.destino : pedido.coordenadas},
                     id_producto = [id_producto],
-                    cantidad = pedido.cantidad
+                    cantidad = capacidad_camiones
                 )
 
                 camiones[camio.id_camion] = camio
 
                 # MODIFICAR LA QUANTIAT QUE QUEDA DEL PEDIDO EN pedidos_ordenados
-                pedidos_ordenados[id_producto][index1].cantidad = capacidad_camiones
+                pedido.cantidad = pedido.cantidad - capacidad_camiones
 
             # SI LA QUANTITAT DEL PEDIDO ES INFERIOR A LA CAPACITAT DELS CAMIONS
-            elif pedido.cantidad < capacidad_camiones:
+            if pedido.cantidad < capacidad_camiones:
 
+                # COMPROVAR SI HI HA CAMIONS CREATS
                 if camiones:
                 
                     # ITERAR PER CAMIONS CREATS PER VEURE SI HI CAP EN ALGÚN CAMIÓ
                     for id_camion, info_camion in list(camiones.items()):
-                        
-                        # SI LA SUMA DE LA QUANTIAT CARREGADA I LA QUANTITAT DEL PEDIDO ES MES GRAN QUE LA CAPACITAT D'UN CAMIÓ
-                        if (info_camion.cantidad + pedido.cantidad) > capacidad_camiones:
+
+                        # COMPROVAR SI EL CAMIÓ JA ESTA PLE
+                        if info_camion.cantidad != capacidad_camiones:
                             
-                            camiones[id_camion].id_pedidos.append(pedido.id_pedido)
-                            camiones[id_camion].destinos[pedido.destino] = pedido.coordenadas
-                            camiones[id_camion].id_productos.append(pedido.id_producto)
+                            # SI LA SUMA DE LA QUANTIAT CARREGADA I LA QUANTITAT DEL PEDIDO ES MES GRAN QUE LA CAPACITAT D'UN CAMIÓ
+                            if (info_camion.cantidad + pedido.cantidad) > capacidad_camiones:
+                                
+                                camiones[id_camion].id_pedidos.append(pedido.id_pedido)
+                                camiones[id_camion].destinos[pedido.destino] = pedido.coordenadas
+                                camiones[id_camion].id_productos.append(pedido.id_producto)
 
-                            pedidos_ordenados[id_producto][index1].cantidad = pedido.cantidad - (capacidad_camiones - info_camion.cantidad)
+                                # LA NOVA QUANTITAT DEL PRODUCTE ES LA RESTA DE LA CAPACITAT QUE FALTA PER OMPLIR DEL CAMIÓ
+                                pedido.cantidad = pedido.cantidad - (capacidad_camiones - info_camion.cantidad)
 
-                            camiones[id_camion].cantidad += (capacidad_camiones - info_camion.cantidad)
+                                # ACTUALITZAR LA QUANTITAT DEL CAMIÓ
+                                camiones[id_camion].cantidad += (capacidad_camiones - info_camion.cantidad)
 
-                            
-                            break
+                                # SI LA QUANTITAT DE LA COMANDA ACTUAL SEGUEIX SUPERANT LA CAPACITAT D'UN CAMIÓ
+                                while pedido.cantidad > 0:
+                                    camio = Camion(
+                                        id_camion = siguiente_id_camion(camiones),
+                                        id_pedidos = [pedido.id_pedido],
+                                        destinos = {pedido.destino : pedido.coordenadas},
+                                        id_productos = [id_producto],
+                                        cantidad = pedido.cantidad
+                                    )
 
-                        # SI LA QUANTITAT CARREGADA AL CAMIÓ I LA QUANTITAT DEL PEDIDO ES INFERIOR A LA CAPACITAT DEL CAMIÓ
-                        elif (info_camion.cantidad + pedido.cantidad) < capacidad_camiones:
+                                    camiones[camio.id_camion] = camio
 
-                            # ACTUALITZAR EL CAMIÓ AMB LA INFO DEL PEDIDO ACTUAL
-                            camiones[id_camion].id_pedidos.append(pedido.id_pedido)
-                            camiones[id_camion].destinos[pedido.destino] = pedido.coordenadas
-                            camiones[id_camion].id_productos.append(pedido.id_producto)
-                            camiones[id_camion].cantidad += pedido.cantidad
+                                    pedido.cantidad = pedido.cantidad - camio.cantidad
 
-                            pedidos_to_pop.append(index1)
+                                break
 
-                            break
+                            # SI LA QUANTITAT CARREGADA AL CAMIÓ I LA QUANTITAT DEL PEDIDO ES INFERIOR A LA CAPACITAT DEL CAMIÓ
+                            elif (info_camion.cantidad + pedido.cantidad) < capacidad_camiones:
 
-                # CREAR EL CAMIÓ
-                else:
+                                # ACTUALITZAR EL CAMIÓ AMB LA INFO DEL PEDIDO ACTUAL
+                                camiones[id_camion].id_pedidos.append(pedido.id_pedido)
+                                camiones[id_camion].destinos[pedido.destino] = pedido.coordenadas
+                                camiones[id_camion].id_productos.append(pedido.id_producto)
+                                camiones[id_camion].cantidad += pedido.cantidad
+
+                                pedidos_to_pop.append(index1)
+
+                                break
+
+                    # CREAR EL CAMIÓ SI NO HI HA CAP CAMIÓ QUE TINGUI MENYS PRODUCTES CARREGATS QUE LA CAPACITAT D'UN CAMIÓ
+                    else:
+                        camio = Camion(
+                            id_camion = siguiente_id_camion(camiones),
+                            id_pedidos = [pedido.id_pedido],
+                            destinos = {pedido.destino : pedido.coordenadas},
+                            id_productos = [id_producto],
+                            cantidad = pedido.cantidad
+                        )
+
+                        camiones[camio.id_camion] = camio
+
+                        pedidos_to_pop.append(index1)
+
+                # SI NO S'HA CREAT CAMIONS
+                elif not camiones:
+
                     camio = Camion(
                         id_camion = siguiente_id_camion(camiones),
                         id_pedidos = [pedido.id_pedido],
@@ -242,29 +294,38 @@ while len(pedidos_ordenados.keys()):
         pedidos_to_pop.reverse()
         for index in pedidos_to_pop:
             pedidos_ordenados[id_producto].pop(index)
+########################################################################################################################
 
-
-# IMPRIMIR ELS CAMIONS CREATS DE NOU
+############################## ESTABLIR MATARÓ COM EL ORIGEN ##############################
 for camio in camiones.values():
-    print(camio, "\n")
 
-print(f"TOTAL CAMIONES: {len(camiones.keys())}\n")
+    destinos_finales = {'Mataró':'41.532521,2.423604'}
+    destinos_finales.update(camio.destinos)
 
-##########################################################################################################################################################
+    camio.id_productos = list(dict.fromkeys(camio.id_productos))
+    camio.destinos = destinos_finales
+
+
+########################################################################################################################
+
+############################## OPTIMIZAR LAS RUTAS ##############################
 
 rutes_optimes = {} # ID PEDIDO : RUTA
 
-G = nx.Graph()
+# ITERAR POR LOS CAMIONES
+for camion_id, detalles in camiones.items():
 
-for id_ped, atributos in pedidos_x_producto.items():
-
-    for provincia, coords_gps in atributos[1].items():
+    G = nx.Graph()
+    
+    # AÑADIR LOS NODOS EN BASE A LOS DESTINOS DEL CAMIÓN
+    for provincia, coords_gps in detalles.destinos.items():
 
         lat, lon = map(float, coords_gps.split(","))
         G.add_node(provincia, pos=(lat, lon))
 
-    for provincia1, coords_gps1 in atributos[1].items():
-        for provincia2, coords_gps2 in atributos[1].items():
+    # AÑADIR LAS ARISTAS ENTRE LOS NODOS CREADOS
+    for provincia1, coords_gps1 in detalles.destinos.items():
+        for provincia2, coords_gps2 in detalles.destinos.items():
 
             if provincia1 != provincia2:
 
@@ -277,24 +338,29 @@ for id_ped, atributos in pedidos_x_producto.items():
 
                 G.add_edge(provincia1, provincia2, weight=coste_total)
 
+    # OBTENER LA RUTA OPTIMA
     ruta_optima_cerrada = approx.traveling_salesman_problem(G, cycle=True, weight='weight')
     
-    # Mostrar la ruta con los costes acumulados
+    # MOSTRAR LA RUTA CON LOS COSTES ACUMULADOS
     ruta_con_costes = mostrar_ruta_con_costes_acumulados(ruta_optima_cerrada, G)
 
-    rutes_optimes[id_ped] = ruta_con_costes
+    # AÑADIR LA RUTA OPTIMA DE CADA CAMIÓN AL DICCIONARIO rutes_optimes
+    rutes_optimes[camion_id] = ruta_con_costes
+
+########################################################################################################################
+
+############################## RESULTATS ##############################
+
+# CAMBIAR LOS ID_PRODUCTOS POR LOS NOMBRES DE PRODUCTO
+for id,truck in camiones.items():
+    for ind, producto in enumerate(list(truck.id_productos)):
+        nombre_producto = cursor.execute(f"SELECT nombre_producto FROM productos WHERE id_producto = {producto}").fetchone()
+
+        truck.id_productos[ind] = str(truck.id_productos[ind]).replace(str(producto), nombre_producto[0])
 
 
-for id_producto, atributos in pedidos_x_producto.items():
-    print(f"\n\nID Producto: {id_producto}")
-    print(f"Nombre Producto: {atributos[0]}")
-    print(f"Destinos: {atributos[1]}")
-    #print(f"Cantidad Total: {atributos[2]}")
-    
-    for id, ruta in rutes_optimes.items():
-        if id == id_producto:
-            print(ruta)
-    
-    print("-" * 50)
+# PASARLO A JSON
+camiones_data = {key: value.to_dict() for key, value in camiones.items()}
 
-# DEFINIR EL NOMBRE DE CAMIONS EN BASE A LA CANTITAT DE PRODUCTES Y RECALULAR TOT
+with open('camiones_data.json', 'w', encoding='utf-8') as f:
+    json.dump(camiones_data, f, indent=4,ensure_ascii=False)
