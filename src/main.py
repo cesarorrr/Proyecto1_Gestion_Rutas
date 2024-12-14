9249164e7b04c5ac1149d577fbd11936f647c1dd
Joan <joanstucom@gmail.com>
2024-12-14 14:17:26 +0100

    archivo main modificado

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def run_app():
+    # Usar subprocess para ejecutar streamlit
+    subprocess.run(["streamlit", "run", "src/modules/ui.py"])
+
+if __name__ == "__main__":
+    run_app()
